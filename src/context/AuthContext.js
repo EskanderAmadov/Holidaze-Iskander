0287a0ab8c9324f6568ccbb96c0b3dfc341f2226
EskanderAmadov <114715507+EskanderAmadov@users.noreply.github.com>
2025-06-29 15:53:39 +0200

    Fix

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState, useContext } from "react";
+import { fetchUserProfile } from "../api/profile/fetchProfile"; 
 
 const AuthContext = createContext();
 
@@ -13,7 +14,7 @@ export const AuthProvider = ({ children }) => {
     }
   });
 
-  const login = (userData) => {
+  const login = async (userData) => {
     if (!userData?.accessToken) {
       console.error("Login: Missing accessToken in the response.");
       return;
@@ -22,6 +23,23 @@ export const AuthProvider = ({ children }) => {
     setUser(userData);
     localStorage.setItem("user", JSON.stringify(userData));
     console.log("Logged in:", userData.name);
+
+
+    try {
+      const profile = await fetchUserProfile(userData.name);
+
+      const updatedUser = {
+        ...userData,
+        avatar: profile.avatar,
+        venueManager: profile.venueManager,
+      };
+
+      setUser(updatedUser);
+      localStorage.setItem("user", JSON.stringify(updatedUser));
+      console.log("User profile synced.");
+    } catch (err) {
+      console.warn("Failed to fetch user profile after login", err);
+    }
   };
 
   const logout = () => {
